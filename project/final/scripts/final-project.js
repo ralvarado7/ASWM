607bf4b6b396c60744c73a82609f9165e4f1b032
Rafael Alvarado <rafael.alvaradosanchez@loop.colum.edu>
2016-05-04 10:35:30 -0500

    resolved circle mas follows the mouse

--- project/final/scripts/final-project.js
+++ project/final/scripts/final-project.js
@@ -1,33 +1,47 @@
 window.addEventListener('load', function(){
 
 	
-	var s = Snap("#svg");
-
+	var s = Snap("#svg")
 	
-	var greenbg = s.rect(0,0, 400,300);
-	greenbg.attr({
-	    fill: "white"
-	});
-	//mask group for rectangle and circle
-	var maskrect = s.rect(0,0, 400,300);   
+	// mask group for rectangle and circle
+	var maskrect = s.rect(0,0, 2000,2000);   
 	maskrect.attr({
 	    fill: "white"
 	});
-	var cirMaskX = 140;
-	var cirMaskY = 130;
-	var maskcircle = s.circle(cirMaskX,cirMaskY, 80);  
 	
+	var maskcircle = s.circle(455,455,30);
+	
+
+	//group rect and circle to mask
 	spotlightmask = s.group(maskrect, maskcircle);
 
 	// Add a black foreground rectangle to apply the mask to.
-	var blackfg = s.rect(0,0, 400,300);
-	// Attch the mask to the black foreground rect.
-	blackfg.attr({
-	    mask: spotlightmask
+	var blackfg = s.rect(0,0, 2000,2000);
+	
+	
+	function createMask(){
+		blackfg.attr({
+		    mask: spotlightmask
+		});	
+	}
+	function deleteMask(){
+		blackfg.node.removeAttribute('mask');	
+		console.log(blackfg);
+		console.log(maskcircle);
+	}
+	//creates the mask and the clickpoint
+	window.addEventListener('click', function(e){
+		createMask();
+		
+		var xPosition = e.clientX;
+	    var yPosition = e.clientY;
+
+	    maskcircle.node.setAttribute('cx', xPosition);
+	    maskcircle.node.setAttribute('cy', yPosition);
+
+	    setTimeout(deleteMask,500);
 	});
 
-
-	
 })
 
 
